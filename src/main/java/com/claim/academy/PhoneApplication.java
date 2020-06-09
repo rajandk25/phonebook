25db71aaa24969b54bd9d8a756d3f6590d815d77
rajandk25 <rajandk25@users.noreply.github.com>
2020-06-09 12:13:48 -0500

    Add comment

--- src/main/java/com/claim/academy/PhoneApplication.java
+++ src/main/java/com/claim/academy/PhoneApplication.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
+/**
+ * This is the main class that runs the project.
+ */
 public class PhoneApplication {
 
 	public static void main(String[] args) {
